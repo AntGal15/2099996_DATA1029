b01619d2ab02319e0249a27032c952f185776f87
AntGal15 <antoine.gallant2@gmail.com>
2024-01-29 13:39:28 -0400

    exercice 7 (devoir complet)

--- Devoir_semaine_2.sql
+++ Devoir_semaine_2.sql
@@ -15,3 +15,6 @@ SELECT title, price FROM titles WHERE title NOT LIKE 'S%' AND title NOT LIKE 'B%
 
 -- exercice 6
 SELECT title, price FROM titles WHERE title NOT LIKE 'S%' AND title NOT LIKE 'B%' AND title LIKE '__f%';
+
+-- exercice 7
+SELECT title, price FROM titles WHERE title LIKE 'A%' OR title LIKE 'B%' OR title LIKE 'C%' OR title LIKE 'D%' OR title LIKE 'E%' OR title LIKE 'F%' OR title LIKE 'G%' OR title LIKE 'H%' OR title LIKE 'I%' OR title LIKE 'J%' ORDER BY title;
