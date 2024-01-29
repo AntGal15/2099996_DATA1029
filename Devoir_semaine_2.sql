f9e000d87b4cf906d6152a249f8c4639d3d6966a
AntGal15 <antoine.gallant2@gmail.com>
2024-01-29 13:38:57 -0400

    Exercice 6

--- Devoir_semaine_2.sql
+++ Devoir_semaine_2.sql
@@ -12,3 +12,6 @@ SELECT title, price FROM titles WHERE title_id NOT LIKE 'SU%' AND title_id NOT L
 
 -- exercice 5
 SELECT title, price FROM titles WHERE title NOT LIKE 'S%' AND title NOT LIKE 'B%' AND title LIKE '_o%' ORDER BY title;
+
+-- exercice 6
+SELECT title, price FROM titles WHERE title NOT LIKE 'S%' AND title NOT LIKE 'B%' AND title LIKE '__f%';
