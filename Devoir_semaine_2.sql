f4a8dca7a6ce25c37f420bfafc181f4b230ab402
AntGal15 <antoine.gallant2@gmail.com>
2024-01-29 13:37:14 -0400

    Exercice 4

--- Devoir_semaine_2.sql
+++ Devoir_semaine_2.sql
@@ -6,3 +6,6 @@ SELECT title, price FROM titles WHERE title LIKE '%computer%' AND title NOT LIKE
 
 -- exercice 3
 SELECT title, price FROM titles WHERE (title LIKE 'SU%' OR title LIKE 'BU%') ORDER BY title;
+
+-- exercice 4
+SELECT title, price FROM titles WHERE title_id NOT LIKE 'SU%' AND title_id NOT LIKE 'BU%';
