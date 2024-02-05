50afaff3f13db4cc27f5b52cc5c13ee549ca7ced
AntGal15 <antoine.gallant2@gmail.com>
2024-02-05 13:13:20 -0400

    Exercice 4

--- Devoir-semaine-3-Antoine.sql
+++ Devoir-semaine-3-Antoine.sql
@@ -12,3 +12,12 @@ left join publishers p on a.city = p.city;
 select a.au_fname as AuthorFirstName, a.au_lname as AuthorLastName, p.pub_name as PublisherName, p.city as City
 from publishers p
 left join authors a on p.city = a.city;
+
+-- Exercice 4
+select a.au_fname as AuthorFirstName, a.au_lname as AuthorLastName, p.pub_name as PublisherName, a.city as City
+from authors a
+left join publishers p on a.city = p.city
+union
+select a.au_fname, a.au_lname, p.pub_name, p.city
+from publishers p
+left join authors a on p.city = a.city;
