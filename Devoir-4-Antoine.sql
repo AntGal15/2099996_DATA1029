27a3d6f453011693ee52875ddde7487b3a4894c7
AntGal15 <antoine.gallant2@gmail.com>
2024-02-13 21:53:13 -0400

    Exercice 3 (devoir complet)

--- Devoir-4-Antoine.sql
+++ Devoir-4-Antoine.sql
@@ -17,3 +17,15 @@ join titleauthor ta on a.au_id = ta.au_id
 join titles t on ta.title_id = t.title_id
 join publishers p on t.pub_id = p.pub_id
 where p.pub_name = 'Eyrolles';
+
+-- exercice 3
+select distinct a.au_fname, a.au_lname
+from authors a
+join titleauthor ta on a.au_id = ta.au_id
+join titles t on ta.title_id = t.title_id
+where t.advance > (
+    select MAX(t.advance)
+    from titles t
+    join publishers p on t.pub_id = p.pub_id
+    where p.pub_name = 'Harmattan'
+)
