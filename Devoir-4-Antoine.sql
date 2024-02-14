c872431bfa8492b3a3a429872e36aa055f5ac42d
AntGal15 <antoine.gallant2@gmail.com>
2024-02-13 21:52:21 -0400

    Exercice 2

--- Devoir-4-Antoine.sql
+++ Devoir-4-Antoine.sql
@@ -9,3 +9,11 @@ where au_id not in (
     inner join titles on titleauthor.title_id = titles.title_id
     where pub_id = 1
 );
+
+-- exercice 2
+select distinct a.au_id, a.au_fname, a.au_lname
+from authors a
+join titleauthor ta on a.au_id = ta.au_id
+join titles t on ta.title_id = t.title_id
+join publishers p on t.pub_id = p.pub_id
+where p.pub_name = 'Eyrolles';
