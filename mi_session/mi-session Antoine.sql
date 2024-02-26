44e7df9182db183fa2361cb189ccebaa5f928ce5
AntGal15 <antoine.gallant2@gmail.com>
2024-02-26 14:15:28 -0400

    exercice 2 question 3 mi-session

--- mi_session/mi-session Antoine.sql
+++ mi_session/mi-session Antoine.sql
@@ -6,10 +6,17 @@ from user u
 join role r on r.id = u.role_id;
 
 -- Question 2
---  Noms et quantités des produits achetés par Oumar Moussa. Le résultat doit avoir la structure suivante (20points) :
+--  Noms et quantités des produits achetés par Oumar Moussa. Le résultat doit avoir la structure suivante (20 points) :
 select p.name, ol.quantity
 from product p
 join orderline ol on ol.product_id = p.id
 join invoice i on i.order_id = ol.order_id
 join user u on u.id = i.customer_id
 where u.full_name = "Oumar Moussa";
+
+-- Question 3
+-- Quel sont les noms de produits dont le fournisseur est basé à Moncton ? (25 points)
+select p.name
+from product p
+join supplier s on s.id = p.supplier_id
+where s.city = "Moncton";
