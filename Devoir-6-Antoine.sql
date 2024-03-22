b0b8bd05f8a4416e59b6cfdc76e69923cfb2a971
AntGal15 <antoine.gallant2@gmail.com>
2024-03-22 13:15:18 -0300

    exercice 2

--- Devoir-6-Antoine.sql
+++ Devoir-6-Antoine.sql
@@ -3,3 +3,13 @@ use library;
 select emp.fname,emp.lname, p.pub_name
 from employees emp
 join publishers as p where emp.pub_id = p.pub_id; 
+
+-- 2.Noms complets des employés les plus rémunérés par employeurs
+select CONCAT(e.fname, ' ', e.lname) as "Nom complet", p.pub_name, e.salary
+from employees e
+join publishers p on e.pub_id = p.pub_id
+where e.salary = (
+	select MAX(salary) 
+	from employees 
+	where pub_id = e.pub_id)
+order by p.pub_name;
