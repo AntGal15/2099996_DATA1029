e1dc4b6147aa0deede594459e848a7c653f6ee2e
AntGal15 <antoine.gallant2@gmail.com>
2024-04-03 13:44:25 -0300

    exercice 4

--- Examen fin_session/pharma.sql
+++ Examen fin_session/pharma.sql
@@ -1,3 +1,5 @@
+-- Examen fin session: Antoine Gallant et Francois savoie
+
 -- exercice 1: voir le fichier Examen_Finale_DATA1029.drawio (fichier pour draw.io)
 
 -- exercice 2
@@ -7,3 +9,9 @@
 -- utilisée après des opérations de manipulation de données pour maintenir la cohérence de la base de données.
 
 -- exercice 3 est dans le fichier root.sql
+
+-- Exercice 4
+SELECT CONCAT(u.firstname, ' ', u.lastname) AS "Nom complet", AVG(TIMESTAMPDIFF(MINUTE, ch.login_date, ch.logout_date)) AS "Duree moyene en minutes"
+FROM users u
+JOIN connection_history ch ON u.id = ch.user_id
+GROUP BY u.id;
--- Examen fin_session/root.sql
+++ Examen fin_session/root.sql
@@ -1,6 +1,14 @@
+-- Examen fin session: Antoine Gallant et Francois savoie
+
 -- exercice 1: voir le fichier Examen_Finale_DATA1029.drawio (fichier pour draw.io)
 
 -- exercice 2 repondu dans le fichier pharma.sql
 
 -- exercice 3:
 create user 'pharma'@'localhost' identified by '1234';
+
+-- exercice 4(permission seulement)
+GRANT SELECT ON epharmacy.users TO 'pharma'@'localhost';
+GRANT SELECT ON epharmacy.connection_history TO 'pharma'@'localhost';
+-- apres avoir executer les commandes
+REVOKE SELECT ON epharmacy.* FROM 'pharma'@'localhost';
