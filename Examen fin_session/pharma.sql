80c9df9adfe4a31ff4cdcca7131513649144cf56
AntGal15 <antoine.gallant2@gmail.com>
2024-04-03 13:41:50 -0300

    exercice 2

--- Examen fin_session/pharma.sql
+++ Examen fin_session/pharma.sql
@@ -1,1 +1,7 @@
 -- exercice 1: voir le fichier Examen_Finale_DATA1029.drawio (fichier pour draw.io)
+
+-- exercice 2
+-- La ligne 12 désactive la vérification des contraintes de clé étrangère, permettant 
+-- l'insertion de données sans contrôle de référence. La ligne 440  active la vérification des contraintes
+-- de clé étrangère, garantissant l'intégrité référentielle des données. Elle doit être 
+-- utilisée après des opérations de manipulation de données pour maintenir la cohérence de la base de données.
--- Examen fin_session/root.sql
+++ Examen fin_session/root.sql
@@ -1,1 +1,3 @@
 -- exercice 1: voir le fichier Examen_Finale_DATA1029.drawio (fichier pour draw.io)
+
+-- exercice 2 repondu dans le fichier pharma.sql
